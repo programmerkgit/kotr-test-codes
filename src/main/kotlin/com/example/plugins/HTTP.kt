415656771e6223ea1014f92761f4819b27686f67
admin <k.kubo.private.mobile@gmail.com>
2021-10-15 03:34:53 +0900

    evry

---
+++ src/main/kotlin/com/example/plugins/HTTP.kt
@@ -0,0 +1,50 @@
+package com.example.plugins
+
+import io.ktor.http.*
+import io.ktor.features.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun Application.configureHTTP() {
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+    install(ConditionalHeaders)
+    install(Compression) {
+        gzip {
+            priority = 1.0
+        }
+        deflate {
+            priority = 10.0
+            minimumSize(1024) // condition
+        }
+    }
+    install(ForwardedHeaderSupport) // WARNING: for security, do not include this if not behind a reverse proxy
+    install(XForwardedHeaderSupport) // WARNING: for security, do not include this if not behind a reverse proxy
+    install(DefaultHeaders) {
+        header("X-Engine", "Ktor") // will send this header with each response
+    }
+    install(HSTS) {
+        includeSubDomains = true
+    }
+    install(HttpsRedirect) {
+        // The port to redirect to. By default 443, the default HTTPS port.
+        sslPort = 443
+        // 301 Moved Permanently, or 302 Found redirect.
+        permanentRedirect = true
+    }
+    install(PartialContent) {
+        // Maximum number of ranges that will be accepted from a HTTP request.
+        // If the HTTP request specifies more ranges, they will all be merged into a single range.
+        maxRangeCount = 10
+    }
+
+}
---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,77 @@
+package com.example.plugins
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.locations.*
+import io.ktor.features.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+import io.ktor.webjars.*
+import java.time.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun Application.configureRouting() {
+    install(Locations) {
+    }
+    install(AutoHeadResponse)
+    install(DoubleReceive)
+
+
+    install(Webjars) {
+        path = "/webjars" //defaults to /webjars
+        zone = ZoneId.systemDefault() //defaults to ZoneId.systemDefault()
+    }
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        get<MyLocation> {
+            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
+        }
+        // Register nested routes
+        get<Type.Edit> {
+            call.respondText("Inside $it")
+        }
+        get<Type.List> {
+            call.respondText("Inside $it")
+        }
+        post("/double-receive") {
+            val first = call.receiveText()
+            val theSame = call.receiveText()
+            call.respondText(first + " " + theSame)
+        }
+        // Static plugin. Try to access `/static/index.html`
+        static("/static") {
+            resources("static")
+        }
+        install(StatusPages) {
+            exception<AuthenticationException> { cause ->
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> { cause ->
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+        }
+        get("/webjars") {
+            call.respondText("<script src='/webjars/jquery/jquery.js'></script>", ContentType.Text.Html)
+        }
+    }
+}
+
+@Location("/location/{name}")
+class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
+@Location("/type/{name}")
+data class Type(val name: String) {
+    @Location("/edit")
+    data class Edit(val type: Type)
+
+    @Location("/list/{page}")
+    data class List(val type: Type, val page: Int)
+}
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
